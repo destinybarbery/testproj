4d199f325b7df7a8ba2c0f0f6a40a1c95c84546e
destinybarbery <destiny.barbery@gmail.com>
2021-10-12 08:46:54 -0400

    new script

--- script.js
+++ script.js
@@ -1,48 +1,59 @@
+
 let numRows = 0;
 let numCols = 0;
-let colorSelected;
+let colorSelected; 
 
+//Adds a row
 function addR() {
+
     alert("Clicked Add Row")
-    let grid = document.getElementById("grid");
-    let newRow = document.createElement("tr");
-    newRow.classList.add("grid-row");
-    grid.appendChild(newRow);
+    
+    // check if columns = 0
+    if(numCols == 0)
+        numCols++
 
-    for (let i = 0; i < numCols.length; i++) {
-        newRow.appendChild(createCell("grid-cell"));
-    }
-}
-function addC() {
-    alert("Clicked Add Col")
+    // insert row
+    var row = grid.insertRow() 
+    grid.appendChild(row);
+
+    // insert cells
+    // loop - columns
+    for (var i = 0; i < numCols; i++) {
+        // Add Cell 
+        var newCell = row.insertCell(i); 
+        var newText = document.createTextNode('CELL'); 
+        newCell.appendChild(newText);
+        newCell.onclick = function() {
+            return changeColor(this.parentElement.rowIndex, this.cellIndex+1)
+        }
+    }   
     numRows++;
-    let grid = document.getElementById("grid");
-    let rows = grid.getElementsByClassName("grid-row");
 
-    for (let i = 0; i < rows.length; i++) {
-        rows[i].appendChild(createCell("grid-cell"));
-    }
 }
 
+//Adds a column
+function addC() {
 
-function removeR() {
-    alert("Clicked Remove Row")
-    let grid = document.getElementById("grid");
-    let lastRow = grid.rows.length - 1;
+    alert("Clicked Add Col")
 
-    for (let i = lastRow; i > 0; i--) {
-        grid.deleteRow(i);
+    // check if rows = 0
+    if(numRows == 0) {
+        return addR()
     }
-}
-function removeC() {
-    alert("Clicked Remove Col")
-    numRows = 1;
-    let grid = document.getElementById("grid");
-    let lastColumn = grid.rows[0].cells.length - 1;
-
-    for (let i = 0; i < grid.rows.length; i++) {
-        for (let j = lastColumn; j > 0; j--) {
-            grid.rows[i].deleteCell(j);
+    
+    // insert cells
+    // outer loop - columns
+    for (var i = numCols++; i < numCols; i++) {
+        // inner loop - rows
+        for (var j=0; j < numRows; j++) {
+            // Add Cell 
+            var newCell = grid.rows[j].insertCell(i); 
+            var newText = document.createTextNode('CELL'); 
+            newCell.appendChild(newText);
+            newCell.onclick = function() {
+                return changeColor(this.parentElement.rowIndex, this.cellIndex+1)
+            }
         }
     }
+
 }
